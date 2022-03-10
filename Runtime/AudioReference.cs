71ea036b4bbbeab19ea1a289ed5e8dbe5af16392
Varga <varguiniano@gmail.com>
2022-03-10 10:00:04 +0100

    - Audio reference drawer replaced by Odin Attributes.

--- Runtime/AudioReference.cs
+++ Runtime/AudioReference.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if ODIN_INSPECTOR_3
+using Sirenix.OdinInspector;
+#endif
+#if UNITY_EDITOR
+using UnityEditor;
+using WhateverDevs.Core.Editor.Utils;
+#endif
 
 namespace WhateverDevs.TwoDAudio.Runtime
 {
@@ -6,11 +15,54 @@ namespace WhateverDevs.TwoDAudio.Runtime
     /// Data class to store the reference to an audio.
     /// </summary>
     [Serializable]
+    [InlineProperty]
     public struct AudioReference
     {
         /// <summary>
         /// Audio string that matches the audio
         /// </summary>
+        #if ODIN_INSPECTOR_3
+        #if UNITY_EDITOR
+        [ValueDropdown(nameof(GetAvailableAudios))]
+        #endif
+        [Searchable]
+        [InfoBox("Library hasn't been found!", InfoMessageType.Error, VisibleIf = nameof(libraryNotFound))]
+        [DisableIf(nameof(libraryNotFound))]
+        [HideLabel]
+        #endif
         public string Audio;
+
+        /// <summary>
+        /// Flag to mark the library not found error.
+        /// </summary>
+        private bool libraryNotFound;
+
+        #if UNITY_EDITOR
+
+        /// <summary>
+        /// Retrieve all available audios.
+        /// </summary>
+        /// <returns>A list of all available audio names.</returns>
+        private List<string> GetAvailableAudios()
+        {
+            AudioLibrary library;
+
+            EditorUtility.DisplayProgressBar("2D Audio", "Looking for audio library...", .5f);
+
+            try
+            {
+                library = AssetManagementUtils.FindAssetsByType<AudioLibrary>().First();
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+
+            libraryNotFound = library == null;
+
+            return libraryNotFound ? null : library.GetAllAudioNames();
+        }
+
+        #endif
     }
 }
